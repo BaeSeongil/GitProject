ee73210a038f2f7b779cae8458ccb7e6a764b0d9
Seongil <bs1922@naver.com>
2022-08-04 22:13:13 +0900

    USERS 추가

---
+++ mainPage/src/main/java/com/project/mainPage/controller/UsersController.java
@@ -0,0 +1,10 @@
+package com.project.mainPage.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/users")
+public class UsersController {
+
+}
---
+++ mainPage/src/main/java/com/project/mainPage/dto/UsersDto.java
@@ -0,0 +1,36 @@
+package com.project.mainPage.dto;
+/*
++----------+-------------+------+-----+-------------------+-------------------+
+| Field    | Type        | Null | Key | Default           | Extra             |
++----------+-------------+------+-----+-------------------+-------------------+
+| userid   | varchar(45) | NO   | PRI | NULL              |                   |
+| username | varchar(45) | NO   |     | NULL              |                   |
+| userpw   | varchar(45) | NO   |     | NULL              |                   |
+| phone    | varchar(20) | NO   | UNI | NULL              |                   |
+| email    | varchar(45) | NO   | UNI | NULL              |                   |
+| birth    | date        | NO   |     | NULL              |                   |
+| address  | varchar(45) | NO   |     | NULL              |                   |
+| signup   | datetime    | YES  |     | CURRENT_TIMESTAMP | DEFAULT_GENERATED |
++----------+-------------+------+-----+-------------------+-------------------+
+ */
+
+import java.util.Date;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import lombok.Data;
+
+@Data
+public class UsersDto {
+	private String userid;
+	private String username;
+	private String userpw;
+	private String phone;
+	private String email;
+	@DateTimeFormat(pattern =  "yyyy-MM-dd")
+	private Date birth;
+	private String address;
+	@DateTimeFormat(pattern =  "yyyy-MM-dd'T'HH:mm:ss")
+	private Date signup;
+	
+}
