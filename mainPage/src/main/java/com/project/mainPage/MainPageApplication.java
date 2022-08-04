84fd0b985cbd61b884be6508bec933bfad5ea3a2
bsi <bs@naver.com>
2022-08-04 10:05:23 +0900

    프로젝트 시작

---
+++ mainPage/src/main/java/com/project/mainPage/MainPageApplication.java
@@ -0,0 +1,13 @@
+package com.project.mainPage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainPageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainPageApplication.class, args);
+	}
+
+}
---
+++ mainPage/src/test/java/com/project/mainPage/MainPageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.mainPage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainPageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
