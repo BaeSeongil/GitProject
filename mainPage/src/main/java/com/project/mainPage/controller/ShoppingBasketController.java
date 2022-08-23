8d0ccdce9ba54fc13af612ee951128256c95e1f9
임준성 <mng6975@naver.com>
2022-08-23 11:59:03 +0900

    다시저장

--- mainPage/src/main/java/com/project/mainPage/controller/ShoppingBasketController.java
+++ mainPage/src/main/java/com/project/mainPage/controller/ShoppingBasketController.java
@@ -1,9 +1,13 @@
 package com.project.mainPage.controller;
 
+import java.util.List;
+
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,15 +15,12 @@ import org.springframework.web.bind.annotation.SessionAttribute;
 
 import com.project.mainPage.dto.ShoppingBasket;
 import com.project.mainPage.dto.UsersDto;
-import com.project.mainPage.mapper.ProductMapper;
 import com.project.mainPage.mapper.ShoppingBasketMappper;
 
 @Controller
 @RequestMapping("/basket")
 public class ShoppingBasketController {
 	
-	@Autowired
-	ProductMapper productMapper;
 	
 	@Autowired
 	ShoppingBasketMappper basketMappper;
@@ -28,16 +29,32 @@ public class ShoppingBasketController {
 			ShoppingBasket basket,
 			@SessionAttribute UsersDto loginUsers,
 			HttpSession session) {
-		if(session.getAttribute("loginUsers")!=null) {
-			//int count = 
+		if(session.getAttribute("loginUsers")!=null) { 
 			basket.setUserid(loginUsers.getUserid());
 			System.out.println(basket);
 			int insert=basketMappper.insertOne(basket);
 			//장바구니 페이지로 이동 
-			
+			if(insert>0) {
+				return "redirect:/basket/list";				
+			}
 		}
-		
-		return "redirect:/";
+		return "redirect:/";			
 	}
+	
+	@GetMapping("/list/{page}")
+	public String list(
+			@PathVariable int page,
+			Model model){
+		List<ShoppingBasket> basketList = basketMappper.selectAll();
+		System.out.println("basketList : "+basketList);
+		model.addAttribute(basketList);
+		return "/basket/list";
+	};
+	
+	
+	
+	
+	
+	
 
 }
--- mainPage/src/main/java/com/project/mainPage/mapper/ShoppingBasketMappper.java
+++ mainPage/src/main/java/com/project/mainPage/mapper/ShoppingBasketMappper.java
@@ -1,5 +1,7 @@
 package com.project.mainPage.mapper;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
 
 import com.project.mainPage.dto.ShoppingBasket;
@@ -11,4 +13,5 @@ public interface ShoppingBasketMappper {
 	int countByid(String userid);
 	int insertOne(ShoppingBasket shoppingBasket);
 	int deleteOne(int basket_id);
+	List<ShoppingBasket> selectAll();
 }
