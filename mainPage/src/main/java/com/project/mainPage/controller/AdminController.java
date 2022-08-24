2ed6ee845deee4bf61bfababb77643033afa04d1
Songtae <onlyyoureyes@naver.com>
2022-08-25 02:12:59 +0900

    검색 및 오류fix 페이징 X

--- mainPage/src/main/java/com/project/mainPage/controller/AdminController.java
+++ mainPage/src/main/java/com/project/mainPage/controller/AdminController.java
@@ -54,6 +54,10 @@ public class AdminController {
 		int count = 0;
 		List<Product> productList=productMapper.searchAllProduct(cri);
 		int productCount = productMapper.productsAllGetTotal(cri);
+		List<Board> boardList= boardMapper.searchAllBoard(cri);
+		int boardCount = boardMapper.boardAllGetTotal(cri);
+		List<Notice> noticeList= noticeMapper.searchAllNotice(cri);
+		int noticeCount = noticeMapper.noticeAllGetTotal(cri);
 		if(!productList.isEmpty()) {
 			model.addAttribute("productList",productList);
 			model.addAttribute("row", row);
@@ -61,34 +65,21 @@ public class AdminController {
 			model.addAttribute("page", page);
 			pagination = new Pagination(page, productCount, "/search/", row);
 			model.addAttribute("pagination", pagination);
-		}else {
-			model.addAttribute("listCheck","empty"); 
-			return "/search"; }
-		
-		List<Board> boardList= boardMapper.searchAllBoard(cri);
-		int boardCount = boardMapper.boardAllGetTotal(cri);
-		if(!boardList.isEmpty()) {
+		}if(!boardList.isEmpty()) {
 			model.addAttribute("boardList",boardList);
 			model.addAttribute("row", row);
 			model.addAttribute("boardCount", boardCount);
 			model.addAttribute("page", page);
-			pagination2 = new Pagination(page, boardCount, "/search/", row);
-			model.addAttribute("pagination2", pagination2);
-		}else {
-			model.addAttribute("listCheck","empty"); 
-			return "/search"; }  
-		
-		List<Notice> noticeList= noticeMapper.searchAllNotice(cri);
-		int noticeCount = noticeMapper.noticeAllGetTotal(cri);
-		if(!noticeList.isEmpty()) {
+			pagination = new Pagination(page, productCount, "/search/", row);
+			model.addAttribute("pagination", pagination);
+		}if(!noticeList.isEmpty()) {
 			model.addAttribute("noticeList",noticeList);
 			model.addAttribute("row", row);
 			model.addAttribute("noticeCount", noticeCount);
 			model.addAttribute("page", page);
-			pagination3 = new Pagination(page, noticeCount, "/search/", row);
-			model.addAttribute("pagination3", pagination3);
+			pagination = new Pagination(page, productCount, "/search/", row);
+			model.addAttribute("pagination", pagination);
 		}else {
-			model.addAttribute("listCheck","empty"); 
 			return "/search"; }
 		count=productCount+boardCount+noticeCount;
 		return "/search";
