00143582d432ca8b48ff43e17df4b0b3d505fead
Songtae <onlyyoureyes@naver.com>
2022-08-25 09:58:59 +0900

    검색끝 및 오류 fixed

--- mainPage/src/main/java/com/project/mainPage/controller/AdminController.java
+++ mainPage/src/main/java/com/project/mainPage/controller/AdminController.java
@@ -43,15 +43,10 @@ public class AdminController {
 			@RequestParam(required = false) String type,
 			@RequestParam(defaultValue = "search") String keyword,
 			@PathVariable int page, Criteria cri, Model model) {
-		System.out.println(keyword);
 		int row = 5;
 		int startRow = (page - 1) * row;
 		cri.setAmount(row);
 		cri.setSkip(startRow);
-		Pagination pagination;
-		Pagination pagination2;
-		Pagination pagination3;
-		int count = 0;
 		List<Product> productList=productMapper.searchAllProduct(cri);
 		int productCount = productMapper.productsAllGetTotal(cri);
 		List<Board> boardList= boardMapper.searchAllBoard(cri);
@@ -60,28 +55,84 @@ public class AdminController {
 		int noticeCount = noticeMapper.noticeAllGetTotal(cri);
 		if(!productList.isEmpty()) {
 			model.addAttribute("productList",productList);
+			model.addAttribute("productCount", productCount);
+			model.addAttribute("page", page);
+		}if(!boardList.isEmpty()) {
+			model.addAttribute("boardList",boardList);
+			model.addAttribute("boardCount", boardCount);
+			model.addAttribute("page", page);
+		}if(!noticeList.isEmpty()) {
+			model.addAttribute("noticeList",noticeList);
+			model.addAttribute("noticeCount", noticeCount);
+			model.addAttribute("page", page);
+		}else {
+			return "/search"; }
+		return "/search";
+	}
+	@GetMapping("/search/product/{page}")
+	public String searchProduct(
+			@RequestParam(defaultValue = "search") String keyword,
+			@PathVariable int page, Criteria cri, Model model) {
+		int row = 10;
+		int startRow = (page - 1) * row;
+		cri.setAmount(row);
+		cri.setSkip(startRow);
+		Pagination pagination;
+		List<Product> productList=productMapper.searchAllProduct(cri);
+		int productCount = productMapper.productsAllGetTotal(cri);
+		if(!productList.isEmpty()) {
+			model.addAttribute("productList",productList);
 			model.addAttribute("row", row);
 			model.addAttribute("productCount", productCount);
 			model.addAttribute("page", page);
-			pagination = new Pagination(page, productCount, "/search/", row);
+			pagination = new Pagination(page, productCount, "/search/product/", row);
 			model.addAttribute("pagination", pagination);
-		}if(!boardList.isEmpty()) {
+		}else {
+			return "/search/product"; }
+		return "/search/product";
+	}
+	@GetMapping("/search/board/{page}")
+	public String searchBoard(
+			@RequestParam(defaultValue = "search") String keyword,
+			@PathVariable int page, Criteria cri, Model model) {
+		int row = 10;
+		int startRow = (page - 1) * row;
+		cri.setAmount(row);
+		cri.setSkip(startRow);
+		Pagination pagination;
+		List<Board> boardList= boardMapper.searchAllBoard(cri);
+		int boardCount = boardMapper.boardAllGetTotal(cri);
+		if(!boardList.isEmpty()) {
 			model.addAttribute("boardList",boardList);
 			model.addAttribute("row", row);
 			model.addAttribute("boardCount", boardCount);
 			model.addAttribute("page", page);
-			pagination = new Pagination(page, productCount, "/search/", row);
+			pagination = new Pagination(page, boardCount, "/search/board/", row);
 			model.addAttribute("pagination", pagination);
-		}if(!noticeList.isEmpty()) {
+		}else {
+			return "/search/board"; }
+		return "/search/board";
+	}
+	@GetMapping("/search/notice/{page}")
+	public String searchNotice(
+			@RequestParam(defaultValue = "search") String keyword,
+			@PathVariable int page, Criteria cri, Model model) {
+		int row = 10;
+		int startRow = (page - 1) * row;
+		cri.setAmount(row);
+		cri.setSkip(startRow);
+		Pagination pagination;
+		List<Notice> noticeList= noticeMapper.searchAllNotice(cri);
+		int noticeCount = noticeMapper.noticeAllGetTotal(cri);
+		if(!noticeList.isEmpty()) {
 			model.addAttribute("noticeList",noticeList);
 			model.addAttribute("row", row);
 			model.addAttribute("noticeCount", noticeCount);
 			model.addAttribute("page", page);
-			pagination = new Pagination(page, productCount, "/search/", row);
+			pagination = new Pagination(page, noticeCount, "/search/notice/", row);
 			model.addAttribute("pagination", pagination);
 		}else {
-			return "/search"; }
-		count=productCount+boardCount+noticeCount;
-		return "/search";
+			return "/search/notice"; }
+		return "/search/notice";
 	}
 }
