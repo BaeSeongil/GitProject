dcc54e433151a28b206b024258f18c09b73b29f3
hannah <c0mingu1pros2s@gmail.com>
2022-08-26 17:57:34 +0900

    상품 이미지 구현, js, 카테 수정

---
+++ mainPage/src/main/resources/static/public/js/product/update.js
@@ -0,0 +1,49 @@
+const updateForm=document.forms.updateForm;
+const checkIdUrl="/product/idCheck/";
+const idHelp=document.getElementById("idHelp");
+const idHelp2=document.getElementById("idHelp2");
+const idHelp3=document.getElementById("idHelp3");
+
+updateForm["category.categoryId"].addEventListener("change",checkId);
+function checkId(){
+	let formCheck=false;
+	let v = updateForm["category.categoryId"].value; //ID 입력되는 값
+	console.log(v);
+	if(v==1 || v==2 || v==3){
+		formCheck=true;
+		idHelp.style.display="inline";
+		idHelp.setAttribute("name","productSize");
+		idHelp2.style.display="none";
+		idHelp2.removeAttribute("name");
+		idHelp3.style.display="none";
+		idHelp3.removeAttribute("name");
+		inputColor.style.display="inline";
+	}else if(v==4){
+		formCheck = true;
+		idHelp.style.display="none";
+		idHelp.removeAttribute("name");
+		idHelp2.setAttribute("name","productSize");
+		idHelp2.style.display="inline";
+		idHelp3.style.display="none";
+		idHelp3.removeAttribute("name");
+		inputColor.style.display="inline";
+	}else if(v==5){
+		formCheck = true;
+		idHelp.style.display="none";
+		idHelp.removeAttribute("name");
+		idHelp2.style.display="none";
+		idHelp2.removeAttribute("name");
+		idHelp3.setAttribute("name","productSize");
+		idHelp3.style.display="inline";
+		inputColor.style.display="inline";
+	}
+	return formCheck; 
+}
+
+updateForm.addEventListener("submit",async(e)=>{
+	e.preventDefault(); 
+	let inputId = checkId();
+	if(inputId){
+	updateForm.submit();
+	}
+});
